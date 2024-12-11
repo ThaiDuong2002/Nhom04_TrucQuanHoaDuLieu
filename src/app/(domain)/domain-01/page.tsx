ef7cadf831c03d01d5193d82ab1697891a0dec12
Thái Dương <105373894+ThaiDuong2002@users.noreply.github.com>
2024-12-12 01:09:17 +0700

    Test

--- src/app/(domain)/domain-01/page.tsx
+++ src/app/(domain)/domain-01/page.tsx
@@ -10,27 +10,26 @@ const Domain01 = () => {
     datasetUrl,
     (data: d3.DSVRowString<string>): DatasetInterface | null => {
       if (!data) return null;
-      console.log(data);
 
       return {
         ProductName: data["Product Name"],
         Brand: data.Brand,
-        ActualPrice: Number(data["Actual Price"]),
-        DiscountPrice: Number(data["Discount Price"]),
+        ActualPrice: Number(data["Actual price"]),
+        DiscountPrice: Number(data["Discount price"]),
         Discounted: Number(data.Discounted),
         Stars: Number(data.Stars),
         Rating: Number(data.Rating),
         Reviews: Number(data.Reviews),
-        RAM: Number(data.RAM),
-        Storage: Number(data.Storage),
-        DisplaySize: Number(data["Display Size"]),
+        RAM: Number(data["RAM (GB)"]),
+        Storage: Number(data["Storage (GB)"]),
+        DisplaySize: Number(data["Display Size (inch)"]),
         Camera: data.Camera,
         Description: data.Description,
         Link: data.Link,
       };
     }
   ).then((data: DatasetInterface[]) => {
-    
+    console.log(data);
   });
   return (
     <div>
