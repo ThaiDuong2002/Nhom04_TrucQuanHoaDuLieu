fb3beecb5453483b5cf49fa1f259018d28463370
Thái Dương <105373894+ThaiDuong2002@users.noreply.github.com>
2024-12-11 13:42:17 +0700

    Home Page Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,48 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex h-[calc(100vh-3.5rem)] items-center justify-center">
+      <div className="grid grid-cols-2 gap-4 w-[80vw] lg:w-[60vw] md:w-[70vw]">
+        <Link href="/domain-01">
+          <Card className="hover:cursor-pointer hover:bg-zinc-100">
+            <CardHeader>
+              <CardTitle>Domain 01</CardTitle>
+              <CardDescription>Domain 01 description</CardDescription>
+            </CardHeader>
+          </Card>
+        </Link>
+        <Link href="/domain-02">
+          <Card className="hover:cursor-pointer hover:bg-zinc-100">
+            <CardHeader>
+              <CardTitle>Domain 02</CardTitle>
+              <CardDescription>Domain 02 description</CardDescription>
+            </CardHeader>
+          </Card>
+        </Link>
+        <Link href="/domain-03">
+          <Card className="hover:cursor-pointer hover:bg-zinc-100">
+            <CardHeader>
+              <CardTitle>Domain 03</CardTitle>
+              <CardDescription>Domain 03 description</CardDescription>
+            </CardHeader>
+          </Card>
+        </Link>
+        <Link href="/domain-04">
+          <Card className="hover:cursor-pointer hover:bg-zinc-100">
+            <CardHeader>
+              <CardTitle>Domain 04</CardTitle>
+              <CardDescription>Domain 04 description</CardDescription>
+            </CardHeader>
+          </Card>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/constants/domain-nav-data.ts
+++ src/constants/domain-nav-data.ts
@@ -4,22 +4,22 @@ const domainNavData: {
   description: string;
 }[] = [
   {
-    title: "Domain 1",
+    title: "Domain 01",
     href: "/domain-01",
     description: "Domain 1 description",
   },
   {
-    title: "Domain 2",
+    title: "Domain 02",
     href: "/domain-02",
     description: "Domain 2 description",
   },
   {
-    title: "Domain 3",
+    title: "Domain 03",
     href: "/domain-03",
     description: "Domain 3 description",
   },
   {
-    title: "Domain 4",
+    title: "Domain 04",
     href: "/domain-04",
     description: "Domain 4 description",
   },
