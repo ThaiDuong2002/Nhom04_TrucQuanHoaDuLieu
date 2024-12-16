3c8e9cfc8dcd48961eec0341862fd29b8be96fba
Thien Nhan <ngthiennhan2002@gmail.com>
2024-12-16 13:49:08 +0700

    Thêm Dashboard Tableau

--- src/components/chart/index.jsx
+++ src/components/chart/index.jsx
@@ -1,5 +1,6 @@
 import BarChart from "./bar-chart";
 import HeatMap02 from "./heat-map-02";
 import HeatMap03 from "./heat-map-03";
+import ScatterPlot04 from "./scatter-plot-04";
 
-export { BarChart, HeatMap02, HeatMap03 };
+export { BarChart, HeatMap02, HeatMap03, ScatterPlot04 };
